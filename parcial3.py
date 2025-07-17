79c0fc14468cbcc9c7870173464d6695640477f8
Tata-Om <anolartem@unal.edu.co>
2025-07-17 07:10:52 -0500

    Componente producto

--- parcial3.py
+++ parcial3.py
@@ -1,5 +1,13 @@
-print("hola")
-print("prueba1")
-print("prueba de repositorio remoto")
+#componente 
+class Product:
+    def __init__(self, name, quantity, price):
+        self.name = name
+        self.quantity = quantity
+        self.price = price
+
+    def __str__(self):
+        return f"Producto: {self.name} | Cantidad: {self.quantity} | Precio: ${self.price:.2f}"
+
+
 
 
