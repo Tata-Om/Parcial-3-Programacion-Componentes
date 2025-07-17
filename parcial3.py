da678c8aa74842197aa994cea062fa7f41450a22
Tata-Om <anolartem@unal.edu.co>
2025-07-17 07:17:29 -0500

    Inventario como componente

--- parcial3.py
+++ parcial3.py
@@ -1,5 +1,5 @@
 #componente 
-class Product:
+class Product: #este producto es un componente que tendrá un nombre, una cantidad y un precio
     def __init__(self, name, quantity, price):
         self.name = name
         self.quantity = quantity
@@ -8,6 +8,20 @@ class Product:
     def __str__(self):
         return f"Producto: {self.name} | Cantidad: {self.quantity} | Precio: ${self.price:.2f}"
 
+#componente2
+class Inventory: #este inventario es un componente y tendrá la lista de productos 
+    def __init__(self):
+        self.products = []
 
+    def add_product(self, product):
+        self.products.append(product)
 
+    def remove_product(self, product_name):
+        self.products = [p for p in self.products if p.name != product_name]
+
+    def list_products(self):
+        for product in self.products:
+            print(product)
+
+#componente3
 
